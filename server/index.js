5d16cbfca4f52b06272354b7ecbb4f1f1b7854d9
Adam Johnston <adam.johnston3679@gmail.com>
2018-02-03 21:21:37 +0000

    initial working version which can be run locally

--- server/index.js
+++ server/index.js
@@ -1,5 +1,54 @@
-import micro, { send } from 'micro'
-import { get, post, router } from 'microrouter'
-import { microGraphql, microGraphiql } from 'apollo-server-micro'
+const micro = require('micro')
+const { get, post, router } = require('microrouter')
+const { microGraphql, microGraphiql } = require('apollo-server-micro')
+const { makeExecutableSchema } = require('graphql-tools')
 
-micro().listen(3000)
+// seed data
+const names = [
+  {
+    id: 0,
+    name: 'Adam',
+  },
+  {
+    id: 1,
+    name: 'Tom',
+  },
+]
+
+const typeDefs = `
+  type Name {
+    id: ID!,
+    name: String!
+  }
+
+  type Query {
+    names: [Name]
+  }
+`
+
+const resolvers = {
+  Query: {
+    names: () => names,
+  },
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+})
+
+const graphqlHandler = microGraphql({ schema })
+const graphiqlHandler = microGraphiql({ endpointURL: '/graphql' })
+
+const server = micro(
+  router(
+    get(`/graphql`, graphqlHandler),
+    post(`/graphql`, graphqlHandler),
+    get(`/graphiql`, graphiqlHandler),
+    (request, response) => micro.send(response, 404, 'not found')
+  )
+)
+
+server.listen(8080)
+
+module.exports = () => {}
