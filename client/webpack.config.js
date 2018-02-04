5dc8ca429bab10e06374e75c6b6bb24824e2b7fc
Adam Johnston <adam.johnston3679@gmail.com>
2018-02-04 17:17:27 +0000

    add a loader to fix an issue with Apollo Link

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,7 +10,20 @@ module.exports = {
   },
 
   module: {
-    rules: [{ test: /\.js$/, use: 'babel-loader' }],
+    rules: [
+      { test: /\.js$/, use: 'babel-loader' },
+
+      // necessary due to a TS issue with a package used by Apollo Link
+      // https://github.com/apollographql/apollo-link/issues/248
+      {
+        test: /node_modules\/apollo-link.*?\/lib\/.*?.js/,
+        loader: 'string-replace-loader',
+        options: {
+          search: 'exports.Observable = Observable',
+          replace: 'exports.Observable = Observable.default',
+        },
+      },
+    ],
   },
 
   plugins: [
