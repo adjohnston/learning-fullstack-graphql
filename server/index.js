36937b7b04df3b6682f8d5fdfb9916f7a964d54b
Adam Johnston <adam.johnston3679@gmail.com>
2018-02-04 18:46:05 +0000

    add cors

--- server/index.js
+++ server/index.js
@@ -7,8 +7,17 @@ const graphqlHandler = microGraphql({ schema })
 const graphiqlHandler = microGraphiql({ endpointURL: '/graphql' })
 
 module.exports = router(
-  get(`/graphql`, graphqlHandler),
-  post(`/graphql`, graphqlHandler),
+  get(`/graphql`, (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    return graphqlHandler(req, res)
+  }),
+
+  post(`/graphql`, (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Request-Method', 'POST')
+    return graphqlHandler(req, res)
+  }),
+
   get(`/graphiql`, graphiqlHandler),
   (request, response) => send(response, 404, 'not found')
 )
