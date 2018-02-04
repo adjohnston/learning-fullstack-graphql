c94ec2e387897237a44d49fb2f1c0a05621a1494
Adam Johnston <adam.johnston3679@gmail.com>
2018-02-04 14:20:04 +0000

    webpack config for the dev server

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,8 @@
 const resolve = require('path').resolve
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: resolve(__dirname, 'src', 'index.js'),
+  entry: resolve(__dirname, 'src/index.js'),
 
   output: {
     filename: 'bundle.js',
@@ -11,4 +12,10 @@ module.exports = {
   module: {
     rules: [{ test: /\.js$/, use: 'babel-loader' }],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, 'src/index.html'),
+    }),
+  ],
 }
