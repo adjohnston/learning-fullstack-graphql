7100bdfdfac464c64f31c9a501c4a45cec9a5044
Adam Johnston <adam.johnston3679@gmail.com>
2018-02-10 14:01:12 +0000

    apollo client makes a preflight request and the following allows for that preflight check to pass

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 const { send } = require('micro')
-const { get, post, router } = require('microrouter')
+const { get, post, options, router } = require('microrouter')
 const { microGraphql, microGraphiql } = require('apollo-server-micro')
 const schema = require('./src/schema')
+
+const preflighHandler = (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+  res.setHeader('Access-Control-Allow-Headers', 'content-type')
+
+  send(res, 200, 'success')
+}
 
 const graphqlHandler = microGraphql({ schema })
 const graphiqlHandler = microGraphiql({ endpointURL: '/graphql' })
 
 module.exports = router(
+  options('/graphql', preflighHandler),
+
   get(`/graphql`, (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     return graphqlHandler(req, res)
