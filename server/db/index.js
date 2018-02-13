f70635b7117c5c208a1ba6fd038ce554ce03b7b6
Adam Johnston <adam.johnston3679@gmail.com>
2018-02-13 20:43:59 +0000

    handle reconnect to mongodb if it didnt connect on its first attempt

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,24 @@
 const mongoose = require('mongoose')
 const db = mongoose.connection
+let timer
 
 mongoose.connect('mongodb://mongodb/db')
-db.once('open', () => console.log('MongoDB is connected'))
+
+db.on('error', ({ message }) => {
+  if (
+    message &&
+    message.match(/failed to connect to server .* on first connect/)
+  ) {
+    timer = setTimeout(() => {
+      console.log('Retrying connection…')
+      mongoose.connect('mongodb://mongodb/db').catch(() => {})
+    }, 5000)
+  }
+})
+
+db.once('connected', () => {
+  clearTimeout(timer)
+  console.log('MongoDB connected')
+})
 
 module.exports = db
