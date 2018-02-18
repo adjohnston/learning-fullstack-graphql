a6e6c8bd663eefa283747ebfd11802b35559ea06
Adam Johnston <adam.johnston3679@gmail.com>
2018-02-18 18:00:20 +0000

    enable cors

--- aws/handler.js
+++ aws/handler.js
@@ -4,6 +4,13 @@ const { graphqlLambda, graphiqlLambda } = require('apollo-server-lambda')
 const schema = require('./src/schema')
 
 module.exports = {
-  graphqlHandler: graphqlLambda({ schema }),
+  graphqlHandler(event, context, callback) {
+    const callbackFilter = (error, output) => {
+      output.headers['Access-Control-Allow-Origin'] = '*'
+      callback(error, output)
+    }
+
+    return graphqlLambda({ schema })(event, context, callbackFilter)
+  },
   graphiqlHandler: graphiqlLambda({ endpointURL: '/dev/graphql' }),
 }
