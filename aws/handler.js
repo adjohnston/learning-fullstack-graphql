57ab7fda5684e61c921adfa286692b9b79e429b1
Adam Johnston <adam.johnston3679@gmail.com>
2018-02-18 17:48:35 +0000

    settings and code necessary for aws

--- aws/handler.js
+++ aws/handler.js
@@ -1,16 +1,9 @@
-'use strict';
+'use strict'
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const { graphqlLambda, graphiqlLambda } = require('apollo-server-lambda')
+const schema = require('./src/schema')
 
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
+module.exports = {
+  graphqlHandler: graphqlLambda({ schema }),
+  graphiqlHandler: graphiqlLambda({ endpointURL: '/dev/graphql' }),
+}
